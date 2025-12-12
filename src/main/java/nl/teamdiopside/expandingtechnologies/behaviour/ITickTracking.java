b781627a391a3732902389d2deffbf8b797b70df
Curry <66020230+curryducker@users.noreply.github.com>
2025-12-12 16:35:43 +0100

    Implement workaround for oversending
    - Fixed packages sending when entering the range between simulation distance and chunk loading distance using a cursed workaround :)

---
+++ src/main/java/nl/teamdiopside/expandingtechnologies/behaviour/ITickTracking.java
@@ -0,0 +1,7 @@
+package nl.teamdiopside.expandingtechnologies.behaviour;
+
+public interface ITickTracking {
+    long expandingtechnologies$getLastTick();
+    void expandingtechnologies$setLastTick(long expandingtechnologies$lastTick);
+    boolean expandingtechnologies$isTicking();
+}
--- src/main/java/nl/teamdiopside/expandingtechnologies/mixin/FactoryPanelBehaviourMixin.java
+++ src/main/java/nl/teamdiopside/expandingtechnologies/mixin/FactoryPanelBehaviourMixin.java
@@ -1,10 +1,16 @@
 package nl.teamdiopside.expandingtechnologies.mixin;
 
+import com.simibubi.create.Create;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelBehaviour;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelBlockEntity;
+import com.simibubi.create.content.logistics.packager.PackagerBlockEntity;
 import com.simibubi.create.content.logistics.packagerLink.LogisticsManager;
+import com.simibubi.create.content.logistics.packagerLink.LogisticsNetwork;
+import net.minecraft.world.level.Level;
+import nl.teamdiopside.expandingtechnologies.behaviour.ITickTracking;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -14,9 +20,12 @@ public abstract class FactoryPanelBehaviourMixin {
     @Shadow
     private int lastReportedUnloadedLinks;
 
+    @Unique
+    private int expandingtechnologies$lastReportedNotTickingLinks;
+
     @Redirect(method = "tickStorageMonitor", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/logistics/factoryBoard/FactoryPanelBehaviour;getLevelInStorage()I"))
     private int injected(FactoryPanelBehaviour instance) {
-        int unloadedLinkCount = instance.getUnloadedLinks();
+        int unloadedLinkCount = instance.getUnloadedLinks() + expandingtechnologies$getNotTickingLinks(instance);
         FactoryPanelBlockEntity panelBE = instance.panelBE();
         if (!panelBE.restocker && unloadedLinkCount == 0 && lastReportedUnloadedLinks != 0) {
             // All links have been loaded, invalidate cache so we can get an accurate summary!
@@ -24,5 +33,32 @@ public abstract class FactoryPanelBehaviourMixin {
             LogisticsManager.SUMMARIES.invalidate(instance.network);
         }
         return instance.getLevelInStorage();
+    }
+    @Redirect(method = "tickStorageMonitor", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/logistics/factoryBoard/FactoryPanelBehaviour;getUnloadedLinks()I"))
+    private int injected2(FactoryPanelBehaviour instance) {
+        expandingtechnologies$lastReportedNotTickingLinks = expandingtechnologies$getNotTickingLinks(instance);
+        // I know this is cursed, but it saves me a few redirects.
+        // Unloaded and not ticking, almost the same right?
+        return instance.getUnloadedLinks() + expandingtechnologies$lastReportedNotTickingLinks;
+    }
+
+    @Unique
+    public int expandingtechnologies$getNotTickingLinks(FactoryPanelBehaviour instance) {
+        Level level = instance.getWorld();
+        if (level == null || level.isClientSide())
+            return expandingtechnologies$lastReportedNotTickingLinks;
+        if (instance.panelBE().restocker) {
+            PackagerBlockEntity pbe = instance.panelBE().getRestockedPackager();
+            return pbe == null ? 1 : 0;
+        }
+
+        LogisticsNetwork logisticsNetwork = Create.LOGISTICS.logisticsNetworks.get(instance.network);
+        if (logisticsNetwork == null)
+            return 0;
+        return Math.toIntExact(logisticsNetwork.loadedLinks.stream()
+                .filter(pos -> pos.dimension() == level.dimension()
+                        && level.getBlockEntity(pos.pos()) instanceof ITickTracking plbe
+                        && !plbe.expandingtechnologies$isTicking())
+                .count());
     }
 }
